fd3272e062883e72f2172c80dd7ee94f73a8baa6
Ninh Giang Nguyen <121984969+ninhgiang225@users.noreply.github.com>
2026-05-14 10:41:49 -0400

    Time: 7 ms (77.72%), Space: 19.4 MB (35.99%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        result = "1"
+        for i in range(n-1):
+            result = self.describe(result)
+        return result
+
+    def describe(self, s):
+        result = []
+        count = 1
+
+        for i in range(1, len(s)):
+            if s[i] == s[i-1]:
+                count += 1
+            else:
+                result.append(f'{count}{s[i-1]}')
+                count = 1
+        
+        result.append(f'{count}{s[-1]}')
+
+        return "".join(result)
